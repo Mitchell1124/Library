f0f86cd3836cf95cf164a4a0c423370db5008232
Mitchell Townsend <townsendm4@outlook.com>
2022-09-06 19:35:02 -0400

    Added an example book "card" to the display and outlined the goals in the js file.

--- Index.js
+++ Index.js
@@ -1,1 +1,30 @@
-console.log('hello world')
+//all of the book objects are going to be stored in an array so add a function 
+//to the script (not the constructor) that can take the users input and store the 
+//new book object into an array.
+
+//write a function that loops through the array and displays each book on the page.
+//You can display them in some sort of table, or each on their own 'card'. It 
+//might help for now to manually add a few books to your array so you can see
+//the display
+
+//add a NEW BOOK button that brings up a form allowing the users to input the details
+//for the new book: author, title, # of pages, wheather its been read.
+
+//add a button on each books display to remove the book from the library
+    //you need to associate your DOM elements witht the actual book objects. 
+    //One easy solution is giving the a data-attribute that corresponds to the 
+    //index of the library array
+
+//add a button on each books display to change its read status
+    //to facilitate this you will want to create the function that toggles
+    //a books read status on your Book prototpe instance
+
+var myLibrary = []
+
+function Book() {
+
+}
+
+function addBookToLibrary() {
+
+}
